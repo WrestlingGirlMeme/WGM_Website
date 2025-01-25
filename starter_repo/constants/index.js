0e9f90dd612fdcbe8abbce0834501e3bac23c3bc
WrestlingGirlMeme <wgmcoin@gmail.com>
2025-01-26 00:34:28 +0400

    Update index.js

--- starter_repo/constants/index.js
+++ starter_repo/constants/index.js
@@ -2,32 +2,32 @@ export const exploreWorlds = [
   {
     id: 'world-1',
     imgUrl: '/planet-01.png',
-    title: 'The Hogwarts',
+    title: 'Chillguy',
   },
   {
     id: 'world-2',
     imgUrl: '/planet-02.png',
-    title: 'The Upside Down',
+    title: 'Pepe',
   },
   {
     id: 'world-3',
     imgUrl: '/planet-03.png',
-    title: 'Kadirojo Permai',
+    title: 'Shiba Inu',
   },
   {
     id: 'world-4',
     imgUrl: '/planet-04.png',
-    title: 'Paradise Island',
+    title: 'Doge Coin',
   },
   {
     id: 'world-5',
     imgUrl: '/planet-05.png',
-    title: 'Hawkins Labs',
+    title: 'Floki',
   },
 ];
 
 export const startingFeatures = [
-  'Find a world that suits you and you want to enter',
+  'Find a match that suits you and you want to enter',
   'Enter the world by reading basmalah to be safe',
   'No need to beat around the bush, just stay on the gas and have fun',
 ];
@@ -71,7 +71,7 @@ export const insights = [
 export const socials = [
   {
     name: 'twitter',
-    url: '/twitter.svg',
+    url: 'https://x.com/wgmcoin/twitter.svg',
   },
   {
     name: 'linkedin',
