090114c2c2c1b8461983a9c6eff39e2a131f2c52
WrestlingGirlMeme <wgmcoin@gmail.com>
2025-01-26 22:41:07 +0400

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => (
             className="w-[24px] h-[24px] object-contain"
             href="https://raydium.io/swap/?inputMint=sol&outputMint=4arWzUHj1ecgyQbKq8Kop76JFnKoLLuxREc428gmpump"
           />
-          <span className="font-normal text-[16px] text-white">
+          <span className="font-normal text-[16px] text-white" href="https://raydium.io/swap/?inputMint=sol&outputMint=4arWzUHj1ecgyQbKq8Kop76JFnKoLLuxREc428gmpump">
             Buy @ Raydium
           </span>
           <link href="https://raydium.io/swap/?inputMint=sol&outputMint=4arWzUHj1ecgyQbKq8Kop76JFnKoLLuxREc428gmpump" />
