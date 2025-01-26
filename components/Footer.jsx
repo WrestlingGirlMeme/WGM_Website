cc82e599a6f8a11f3bcda5f1dd23a34379f21f31
WrestlingGirlMeme <wgmcoin@gmail.com>
2025-01-26 22:24:49 +0400

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -17,10 +17,9 @@ const Footer = () => (
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
       <div className="flex items-center justify-between flex-wrap gap-5">
         <h4 className="font-bold md:text-[64px] text-[44px] text-white">
-          Buy $WGM ->
+          Enter the Metaverse
         </h4>
         <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
-         
           <img
             src="/headset.svg"
             alt="headset"
