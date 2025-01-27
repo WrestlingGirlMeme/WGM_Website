eaf5c0f90e38e378824e64d0a1d6bbc6cbb71089
WrestlingGirlMeme <wgmcoin@gmail.com>
2025-01-27 22:45:00 +0400

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -33,8 +33,10 @@ export const startingFeatures = [
   'Download Phantom Wallet',
   'Write down your seed-phrase and store it on a safe place',
   'Send Sol to your Wallet from a exchange like Binance',
-  'Send Sol to your Wallet from a exchange like Binance',
-  <a href="https://solscan.io/token/4arWzUHj1ecgyQbKq8Kop76JFnKoLLuxREc428gmpump">buy</a>
+   <a href="https://raydium.io/swap/?inputMint=sol&outputMint=4arWzUHj1ecgyQbKq8Kop76JFnKoLLuxREc428gmpump">Click here to go to raydium </a>
+  'Enter the Amount of Sol what you want to invest and click swap',
+    'sign the transaction in your wallet',
+ 
 ];
 
 export const newFeatures = [
