6722910953e7bc9357abde678c1cdfcfa0036bf5
WrestlingGirlMeme <wgmcoin@gmail.com>
2025-01-27 09:02:36 +0400

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,64 +1,46 @@
-'use client';
+<div className="footer-gradient" />
+<div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
+  <div className="flex items-center justify-between flex-wrap gap-5">
+    <h4 className="font-bold md:text-[64px] text-[44px] text-white">
+      Enter the Metaverse
+    </h4>
+    <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
+      <img
+        src="/headset.svg"
+        alt="headset"
+        className="w-[24px] h-[24px] object-contain"
+      />
+      <span className="font-normal text-[16px] text-white">
+        Enter Metaverse
+      </span>
+    </button>
+  </div>
 
-import { motion } from 'framer-motion';
-import { socials } from '../constants';
+  <div className="flex flex-col">
+    <div className="mb-[50px] h-[2px] bg-white opacity-10" />
 
-import styles from '../styles';
-import { footerVariants } from '../utils/motion';
-
-const Footer = () => (
-  <motion.footer
-    variants={footerVariants}
-    initial="hidden"
-    whileInView="show"
-    className={`${styles.xPaddings} py-8 relative`}
-  >
-    <div className="footer-gradient" />
-    <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
-      <div className="flex items-center justify-between flex-wrap gap-5">
-        <h4 className="font-bold md:text-[64px] text-[44px] text-white">
-          Buy $WGM
-        </h4>
-        <button href="https://raydium.io/swap/?inputMint=sol&outputMint=4arWzUHj1ecgyQbKq8Kop76JFnKoLLuxREc428gmpump" type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
-          <img
-            src="/headset.svg"
-            alt="headset"
-            className="w-[24px] h-[24px] object-contain"
-            href="https://raydium.io/swap/?inputMint=sol&outputMint=4arWzUHj1ecgyQbKq8Kop76JFnKoLLuxREc428gmpump"
-          />
-          <span className="font-normal text-[16px] text-white" href="https://raydium.io/swap/?inputMint=sol&outputMint=4arWzUHj1ecgyQbKq8Kop76JFnKoLLuxREc428gmpump">
-            <link href="https://raydium.io/swap/?inputMint=sol&outputMint=4arWzUHj1ecgyQbKq8Kop76JFnKoLLuxREc428gmpump" />
-            Buy @ Raydium
-          </span>
-          <link href="https://raydium.io/swap/?inputMint=sol&outputMint=4arWzUHj1ecgyQbKq8Kop76JFnKoLLuxREc428gmpump" />
-        </button>
-      </div>
-
-      <div className="flex flex-col">
-        <div className="mb-[50px] h-[2px] bg-white opacity-10" />
-
-        <div className="flex items-center justify-between flex-wrap gap-4">
-          <h4 className="font-extrabold text-[24px] text-white">
-            Wrestling Girl Meme
-          </h4>
-          <p className="font-normal text-[14px] text-white opacity-50">
-            Copyright © 2024 - 2025 $WGM Coin. All rights reserved.
-          </p>
-
-          <div className="flex gap-4">
-            {socials.map((social) => (
-              <img
-                key={social.name}
-                src={social.url}
-                alt={social.name}
-                className="w-[24px] h-[24px] object-contain cursor-pointer"
-              />
-            ))}
-          </div>
-        </div>
-      </div>
+    <div className="flex items-center justify-between flex-wrap gap-4">
+      <h4 className="font-extrabold text-[24px] text-white">
+            METAVERUS
+    </h4>
+    <p className="font-normal text-[14px] text-white opacity-50">
+        Copyright © 2021 - 2022 Metaversus. All rights reserved.
+    </p>
+    
+    <div className="flex gap-4">
+        {socials.map((social) => (
+            <a key={social.name} href={social.link} target="_blank" rel="noopener noreferrer">
+                <img
+                    src={social.url}
+                    alt={social.name}
+                    className="w-[24px] h-[24px] object-contain cursor-pointer"
+                />
+            </a>
+        ))}
+        <a href="https://raydium.io/swap/?inputMint=sol&outputMint=4arWzUHj1ecgyQbKq8Kop76JFnKoLLuxREc428gmpump" target="_blank" rel="noopener noreferrer" className="text-white">
+            Swap on Raydium
+        </a>
     </div>
-  </motion.footer>
-);
-
-export default Footer;
+</div>
+  </div>
+</div>
