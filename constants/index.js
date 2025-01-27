88420092dd0e5fcf829b9190be84259b589da0c3
WrestlingGirlMeme <wgmcoin@gmail.com>
2025-01-27 09:26:05 +0400

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -39,80 +39,53 @@ export const newFeatures = [
   {
     imgUrl: '/vrpano.svg',
     title: 'A new world',
-    subtitle: 'we have the latest update with new world for you to try never mind',
+    subtitle:
+        'we have the latest update with new world for you to try never mind',
   },
   {
     imgUrl: '/headset.svg',
     title: 'More realistic',
-    subtitle: 'In the latest update, your eyes are narrow, making the world more realistic than ever',
+    subtitle:
+        'In the latest update, your eyes are narrow, making the world more realistic than ever',
   },
 ];
 
 export const insights = [
   {
     imgUrl: '/planet-06.png',
-    title: 'The launch of the Metaverse makes Elon Musk ketar-ketir',
-    subtitle: 'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
+    title: 'The launch of the Metaverse makes Elon musk ketar-ketir',
+    subtitle:
+        'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
   },
   {
     imgUrl: '/planet-07.png',
     title: '7 tips to easily master the madness of the Metaverse',
-    subtitle: 'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
+    subtitle:
+        'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
   },
   {
     imgUrl: '/planet-08.png',
     title: 'With one platform you can explore the whole world virtually',
-    subtitle: 'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
+    subtitle:
+        'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
   },
 ];
 
 export const socials = [
   {
     name: 'twitter',
-    url: 'https://twitter.com',
-    imgUrl: '/twitter.svg',
+    url: '/twitter.svg',
   },
   {
     name: 'linkedin',
-    url: 'https://linkedin.com',
-    imgUrl: '/linkedin.svg',
+    url: '/linkedin.svg',
   },
   {
     name: 'instagram',
-    url: 'https://instagram.com',
-    imgUrl: '/instagram.svg',
+    url: '/instagram.svg',
   },
   {
     name: 'facebook',
-    url: 'https://facebook.com',
-    imgUrl: '/facebook.svg',
+    url: '/facebook.svg',
   },
 ];
-
-// Social Links component
-const SocialLinks = () => (
-  <div>
-    {socials.map((social) => (
-      <a href={social.url} key={social.name} target="_blank" rel="noopener noreferrer">
-        <img src={social.imgUrl} alt={social.name} style={{ margin: '0 10px' }} />
-      </a>
-    ))}
-  </div>
-);
-
-// Main App component
-const App = () => (
-  <div>
-    <h1>Explore Worlds</h1>
-    {exploreWorlds.map(world => (
-      <div key={world.id}>
-        <img src={world.imgUrl} alt={world.title} />
-        <h2>{world.title}</h2>
-      </div>
-    ))}
-    <h2>Social Media Links</h2>
-    <SocialLinks />
-  </div>
-);
-
-export default App;
