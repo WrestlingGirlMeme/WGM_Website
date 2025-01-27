955f17ac140d97c7b4f45a7a5b1f4295cdc0af96
WrestlingGirlMeme <wgmcoin@gmail.com>
2025-01-27 09:08:06 +0400

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -19,16 +19,18 @@ const Footer = () => (
         <h4 className="font-bold md:text-[64px] text-[44px] text-white">
           Enter the Metaverse
         </h4>
-        <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
-          <img
-            src="/headset.svg"
-            alt="headset"
-            className="w-[24px] h-[24px] object-contain"
-          />
-          <span className="font-normal text-[16px] text-white">
-            Enter Metaverse
-          </span>
-        </button>
+        <a href="https://raydium.io/swap/?inputMint=sol&outputMint=4arWzUHj1ecgyQbKq8Kop76JFnKoLLuxREc428gmpump" target="_blank" rel="noopener noreferrer">
+          <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
+            <img
+              src="/headset.svg"
+              alt="headset"
+              className="w-[24px] h-[24px] object-contain"
+            />
+            <span className="font-normal text-[16px] text-white">
+              Swap on Raydium
+            </span>
+          </button>
+        </a>
       </div>
 
       <div className="flex flex-col">
@@ -52,9 +54,6 @@ const Footer = () => (
                 />
               </a>
             ))}
-            <a href="https://raydium.io/swap/?inputMint=sol&outputMint=4arWzUHj1ecgyQbKq8Kop76JFnKoLLuxREc428gmpump" target="_blank" rel="noopener noreferrer" className="text-white">
-              Swap on Raydium
-            </a>
           </div>
         </div>
       </div>
