1af9323f760597f0f305ef5afebb80a0bfc4b864
WrestlingGirlMeme <wgmcoin@gmail.com>
2025-01-28 09:24:19 +0400

    Update World.jsx

--- sections/World.jsx
+++ sections/World.jsx
@@ -16,11 +16,10 @@ const World = () => (
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
 
-      <TypingText title="| People on the World" textStyles="text-center" />
+      <TypingText title="| Smashing around the world" textStyles="text-center" />
       <TitleText
         title={(
-          <>Track friends around you and invite them to play together in the same
-            world
+          <> $WGM is Smashing the world of meme 
           </>
         )}
         textStyles="text-center"
