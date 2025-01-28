647f8c753b2cdcc39ccf5c747798de2117d13c1d
WrestlingGirlMeme <wgmcoin@gmail.com>
2025-01-28 11:37:43 +0400

    Update World.jsx

--- sections/World.jsx
+++ sections/World.jsx
@@ -32,23 +32,23 @@ const World = () => (
         <img src="/map.png" alt="map" className="w-full h-full object-cover" />
 
         <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
-          <img src="pepe.png" alt="people" className="w-full h-full" />
+          <img src="pepe.png" alt="pepe" className="w-full h-full" />
         </div>
 
         <div className="absolute top-10 left-[55%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
-          <img src="floki2.png" alt="people" className="w-full h-full" /> 
+          <img src="floki2.png" alt="floki" className="w-full h-full" /> 
            </div>
 
         <div className="absolute top-10 left-[5%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
-          <img src="/trump.png" alt="people" className="w-full h-full" />
+          <img src="/trump.png" alt="trump" className="w-full h-full" />
         </div>
 
         <div className="absolute top-2/3 left-[15%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
-          <img src="shiba1.png" alt="people" className="w-full h-full" />
+          <img src="shiba1.png" alt="shiba" className="w-full h-full" />
         </div>
 
         <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
-          <img src="doge1.png" alt="people" className="w-full h-full" />
+          <img src="doge1.png" alt="doge" className="w-full h-full" />
         </div>
       </motion.div>
     </motion.div>
