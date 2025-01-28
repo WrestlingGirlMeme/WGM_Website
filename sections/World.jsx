0d50ef7c64fee8b8316427deed0cdadd899068eb
WrestlingGirlMeme <wgmcoin@gmail.com>
2025-01-28 11:07:39 +0400

    Update World.jsx

--- sections/World.jsx
+++ sections/World.jsx
@@ -43,7 +43,7 @@ const World = () => (
           <img src="/shiba1.png" alt="people" className="w-full h-full" />
         </div>
 
-        <div className="absolute top-1/2 left-[15%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
+        <div className="absolute top-2/3 left-[15%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
           <img src="floki2.png" alt="people" className="w-full h-full" />
         </div>
 
